01e06cd063d0272307b1e5db2724286e21c21092
cafemug <hyanghope@naver.com>
2020-08-29 15:46:54 +0900

    구현

--- websoc.py
+++ websoc.py
@@ -1,5 +1,5 @@
 import os
-from json import dump
+from json import dumps
 import asyncio
 import websockets
 from telegram import send_telegram
@@ -54,7 +54,7 @@ class Server:
         chat_data = new_crawl(data["link"])
         chat_data["result"] = data["result"]
         send_telegram(data)
-        await websocket.send(json.dumps(chat_data).encode('utf-8'))
+        await websocket.send(dumps(chat_data).encode('utf-8'))
 
 if __name__ == '__main__':
   ws = Server()
